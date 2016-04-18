1a65834ebc4771763b3162e112ad587bacedfafc
colbysadams <colbysadams@gmail.com>
2016-04-17 19:50:48 -0500

    got a basic connection to the mysql database up and running in Main class

--- src/csc315finalproject/CSC315FinalProjectMain.java
+++ src/csc315finalproject/CSC315FinalProjectMain.java
@@ -5,6 +5,11 @@
  */
 package csc315finalproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author colbysadams
@@ -17,7 +22,40 @@ public class CSC315FinalProjectMain
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
-    }
 
+        try
+        {
+            //System.out.println("0");
+            String Query = "select * from AirPort_T order by AirportCode";
+            //System.out.println("1");
+            String insert = "insert into Airport_T values ('MSY', 'Louis Armstrong','New Orleans', 'USA', 5)";
+            //System.out.println("2");
+            Class.forName("com.mysql.jdbc.Driver");
+            //System.out.println("3");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/AirTravel", "CSC315USER", "password");
+            System.out.println("4");
+            Statement statement = connect.createStatement();
+            System.out.println("5");
+            //st.execute(insert);
+            System.out.println("6");
+            ResultSet resultSet = statement.executeQuery(Query);
+
+            while (resultSet.next())
+            {
+
+                String sname = resultSet.getString("AirportCode");
+
+                System.out.println(sname);
+            }
+            resultSet.close();
+            statement.close();
+            connect.close();
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("crap...");
+        }
+
+    }
 }
